64a813e7577c4871b8ec88cb9c3d188132458dda
Peter Daniel <petrus.daniel@protonmail.com>
2022-05-05 20:48:33 +0200

    Removed an img

--- src/home.js
+++ src/home.js
@@ -22,7 +22,6 @@ gsap
   .fromTo(".cloud1", { y: 100 }, { y: -800 }, 0)
   .fromTo(".cloud2", { y: -150 }, { y: -500 }, 0)
   .fromTo(".cloud3", { y: -50 }, { y: -650 }, 0)
-  .fromTo(".trees", { y: 100 }, { y: -650 }, 0)
   .fromTo(".mountBg", { y: -10 }, { y: -100 }, 0)
   .fromTo(".mountMg", { y: -30 }, { y: -250 }, 0)
   .fromTo(".mountFg", { y: -50 }, { y: -600 }, 0);
